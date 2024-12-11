5b32ba03de28dcea24b562e0e5409771202c1968
Hanasolo <hanasolo@hotmail.com>
2024-12-11 16:16:23 +0100

    functions to the home score added

--- main.js
+++ main.js
@@ -1,4 +1,25 @@
 
 // aqui las funciones
+let initialHomeScore = 0
+let btnAddOne =  1
+let btnAddTwo =  2
+let btnAddThree =  3
+document.getElementById('number-box-home').textContent = initialHomeScore
 
 
+let homeScoreEl = document.getElementById('number-box-home')
+
+function addOne() {
+    let result = initialHomeScore += btnAddOne
+    return homeScoreEl.textContent = result
+}
+
+function addTwo() {
+    let result = initialHomeScore += btnAddTwo
+    return homeScoreEl.textContent = result
+}
+
+function addThree() {
+    let result = initialHomeScore += btnAddThree
+    return homeScoreEl.textContent = result
+}
