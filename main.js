3af6f75e95c17c5481e43089263616dcd388703f
Hanasolo <hanasolo@hotmail.com>
2024-12-12 06:31:23 +0100

    finished

--- main.js
+++ main.js
@@ -1,11 +1,12 @@
 
 // aqui las funciones
 let initialHomeScore = 0
+let initialGuestScore = 0
 let btnAddOne =  1
 let btnAddTwo =  2
 let btnAddThree =  3
 document.getElementById('number-box-home').textContent = initialHomeScore
-
+document.getElementById('number-box-guest').textContent = initialGuestScore
 
 let homeScoreEl = document.getElementById('number-box-home')
 
@@ -22,4 +23,23 @@ function addTwo() {
 function addThree() {
     let result = initialHomeScore += btnAddThree
     return homeScoreEl.textContent = result
+}
+
+/*------------------Guest-------------------------------*/
+
+let guestScoreEl = document.getElementById('number-box-guest')
+
+function addOneGuest() {
+    let result = initialHomeScore += btnAddOne
+    return guestScoreEl.textContent = result
+}
+
+function addTwoGuest() {
+    let result = initialHomeScore += btnAddTwo
+    return guestScoreEl.textContent = result
+}
+
+function addThreeGuest() {
+    let result = initialHomeScore += btnAddThree
+    return guestScoreEl.textContent = result
 }
